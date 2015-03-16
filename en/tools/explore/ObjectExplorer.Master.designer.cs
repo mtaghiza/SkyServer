7d2fda0c0e29d517253f5f712c3afabc2df0e5e6
raddick <raddick@jhu.edu>
2015-03-16 13:54:35 -0400

    Fake login control on Explore tool
    See the new .ascx file and associated in the tools/explore directory. I
    also added an id called "loginbox" to tools.css.

--- en/tools/explore/ObjectExplorer.Master.designer.cs
+++ en/tools/explore/ObjectExplorer.Master.designer.cs
@@ -31,6 +31,15 @@ namespace SkyServer.Tools.Explore {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// loginbox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::SkyServer.en.tools.explore.WebUserControl1 loginbox1;
+        
+        /// <summary>
         /// OEContent control.
         /// </summary>
         /// <remarks>
---
+++ en/tools/explore/login.ascx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SkyServer.en.tools.explore
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+   {
+        public bool loggedin = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void logUserIn(object sender, EventArgs e)
+        {
+            loggedin = true;
+
+        }
+
+        protected void logUserOut(object sender, EventArgs e)
+        {
+            loggedin = false;
+
+        }
+    }
+
+}
